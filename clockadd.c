daa350f04249d0674775837a5dc5c926f3b96359
Roland Dieterich <roland.dieterich@zeitsenke.de>
2017-11-23 21:19:24 +0100

    Use clock_gettime and clock_settime.

--- clockadd.c
+++ clockadd.c
@@ -1,6 +1,5 @@
-#include <sys/types.h>
-#include <sys/time.h>
 #include <sys/select.h>
+#include <time.h>
 #include <unistd.h>
 #include "substdio.h"
 #include "strerr.h"
@@ -9,14 +8,14 @@
 #define FATAL "clockadd: fatal: "
 
 unsigned char buf[16];
-struct timeval tv;
+struct timespec tv;
 
 int main(int argc, char **argv)
 {
   unsigned long u;
   unsigned long v;
   long adj;
-  struct timeval tvselect;
+  struct timespec tvselect;
 
   if (read(0,buf,sizeof buf) != sizeof buf)
     strerr_die2x(111,FATAL,"data split across packets");
@@ -35,24 +34,23 @@ int main(int argc, char **argv)
   v <<= 8; v += buf[9];
   v <<= 8; v += buf[10];
   v <<= 8; v += buf[11];
-  v /= 1000;
 
   /* XXX: Solaris stupidity */
-  gettimeofday(&tv,(struct timezone *) 0);
+  clock_gettime(CLOCK_REALTIME, &tv);
   tvselect.tv_sec = 0;
-  tvselect.tv_usec = 1000000 - ((tv.tv_usec + v) % 1000000);
-  select(1,(fd_set *) 0,(fd_set *) 0,(fd_set *) 0,&tvselect);
+  tvselect.tv_nsec = 1000000000L - ((tv.tv_nsec + v) % 1000000000L);
+  pselect(1, (fd_set *) 0, (fd_set *) 0, (fd_set *) 0, &tvselect, NULL);
 
-  gettimeofday(&tv,(struct timezone *) 0);
+  clock_gettime(CLOCK_REALTIME, &tv);
   tv.tv_sec += adj;
-  tv.tv_usec += v;
-  if (tv.tv_usec > 999999) {
-    tv.tv_usec -= 1000000;
+  tv.tv_nsec += v;
+  if (tv.tv_nsec > 999999999L) {
+    tv.tv_nsec -= 1000000000L;
     tv.tv_sec += 1;
   }
 
-  if (settimeofday(&tv,(struct timezone *) 0) == -1)
-    strerr_die2sys(111,FATAL,"unable to settimeofday: ");
+  if (clock_settime(CLOCK_REALTIME, &tv) == -1)
+    strerr_die2sys(111,FATAL,"unable to clock_settime: ");
 
   _exit(0);
   return 0;
